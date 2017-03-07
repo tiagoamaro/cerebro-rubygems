b813b995b14ee08ff2bbd9b3423b903236ea85ba
Tiago Amaro <tiagopadrela@gmail.com>
2017-03-07 10:22:28 +0100

    search Rubygems through Cerebro

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 'use strict';
 
+const ICON_FILE      = require('./icon.png');
+const RUBYGEMS_REGEX = /rubygems\s+(.*)/i;
+
 const plugin = ({term, display, actions}) => {
-  // It is your main plugin function
-  // do something and call display() with your results
+  if (RUBYGEMS_REGEX.test(term)) {
+    const openBrowser = (url) => {
+      actions.open(url);
+      actions.hideWindow()
+    };
+
+    const searchRubygems = (query) => {
+      let q   = encodeURIComponent(query);
+      let url = `https://rubygems.org/search?query=${q}`;
+      openBrowser(url);
+    };
+
+    let [, RubygemsQuery] = RUBYGEMS_REGEX.exec(term);
+
+    display({
+      icon:     ICON_FILE,
+      title:    `Search Rubygems for ${RubygemsQuery}`,
+      onSelect: () => searchRubygems(RubygemsQuery),
+//      getPreview: () => <Preview query={RubygemsQuery} key={RubygemsQuery} openBrowser={openBrowser}/>
+    })
+  }
 };
 
 module.exports = {
   fn: plugin
-}
+};
